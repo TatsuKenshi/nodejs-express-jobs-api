5272ca43d3b20d5df3a966fe7448940b3e1ffb8e
TatsuKenshi <rockinbg@gmail.com>
2023-08-30 16:56:16 +0200

    lowers the request rate limit to 50 every 10 minutes

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 10 * 60 * 1000, // 15 minutes
+    max: 50, // limit each IP to 100 requests per windowMs
   })
 );
 app.use(express.json());
