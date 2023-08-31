8f792e777b0fc66cd0eeaf0153ff7a4a71e4c205
TatsuKenshi <rockinbg@gmail.com>
2023-08-31 23:04:49 +0200

    adds swagger setup

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // express
 const express = require("express");
 const app = express();
@@ -26,14 +31,19 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
-    windowMs: 10 * 60 * 1000, // 15 minutes
-    max: 50, // limit each IP to 100 requests per windowMs
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 50, // limit each IP to 50 requests per windowMs
   })
 );
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send("<h1>jobs API</h1><a href='/api-docs'>Documentation</a>");
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
