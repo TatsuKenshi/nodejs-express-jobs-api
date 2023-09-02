88b87a8991fe3ddef17648b76769cb3e1e5dcb70
TatsuKenshi <rockinbg@gmail.com>
2023-09-02 22:31:27 +0200

    Custom error messages improved

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,7 +3,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || "Something went wrong, try again later",
+    msg: err.message || "Something went wrong, please try again later.",
   };
 
   if (err.name === "ValidationError") {
@@ -17,12 +17,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
-    )} field, please choose another value`;
+    )} field, please choose another value.`;
     customError.statusCode = 400;
   }
 
   if (err.name === "CastError") {
-    customError.msg = `No item found with id: ${err.value}`;
+    customError.msg = `No item found with id: ${err.value}. Please try another one.`;
     customError.statusCode = 404;
   }
 
